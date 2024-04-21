76b561014ed58a799d90fce7b273651859941888
Darrenwwk <darrenwongwwk@gmail.com>
2024-04-21 20:46:16 +0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,102 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 
-const inter = Inter({ subsets: ["latin"] });
+interface StyleType {
+    backgroundColor?: string; // Optional
+    textColor?: string; // Optional
+    fontFamily?: string; // Optional
+    fontSize?: number; // Optional
+    fontWeight?: number | string; // Optional
+    fontStyle?: string; // Optional
+}
+
+interface WheelData {
+    option?: string;
+    image?: ImageProps;
+    style?: StyleType; // Optional
+    optionSize?: number; // Optional
+}
+
+interface ImageProps {
+    uri: string;
+    offsetX?: number; // Optional
+    offsetY?: number; // Optional
+    sizeMultiplier?: number; // Optional
+    landscape?: boolean; // Optional
+}
+
+const data: WheelData[] = [
+    {
+        option: '海底捞',
+        style: { backgroundColor: '', textColor: 'black' },
+    },
+    { option: 'Sushi Mentai', style: { backgroundColor: 'white' } },
+    { option: '鼎泰丰' },
+    { option: 'bird bird' },
+    { option: 'Mookata' },
+    { option: 'Banana Bro' },
+    { option: 'KFC' },
+    { option: 'McDonald' },
+    { option: 'Texas' },
+    { option: 'Pizza Hut' },
+    { option: 'Subway' },
+    { option: '板面' },
+    { option: 'Thai Food' },
+    { option: '餐馆' },
+    { option: '肉骨茶' },
+    { option: '麻辣烫' },
+    { option: 'Thong Kee' },
+    { option: '咖啡店' },
+    { option: '🐔饭' },
+    { option: '点心' },
+    { option: 'Mamak' },
+    { option: 'Sungai Long Buffet' },
+];
+
+const Wheel = dynamic(() => import('react-custom-roulette').then((mod) => mod.Wheel), { ssr: false });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
+
+    const handleSpinClick = () => {
+        if (!mustSpin) {
+            const newPrizeNumber = Math.floor(Math.random() * data.length);
+            setPrizeNumber(newPrizeNumber);
+            setMustSpin(true);
+        }
+    };
+
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-white py-2">
+            <Wheel
+                mustStartSpinning={mustSpin}
+                prizeNumber={prizeNumber}
+                data={data}
+                onStopSpinning={() => {
+                    setMustSpin(false);
+                    toast.success('Today eat: ' + data[prizeNumber].option, {
+                        position: 'bottom-center',
+                        duration: 5000,
+                        style: {
+                            border: '1px solid green',
+                            padding: '16px',
+                            color: 'white',
+                            backgroundColor: 'green',
+                        },
+                        iconTheme: {
+                            primary: 'green',
+                            secondary: 'white',
+                        },
+                    });
+                }}
+                fontWeight={500}
+                fontSize={15}
             />
-          </a>
+            <button onClick={handleSpinClick} className="text-white bg-black px-4 py-2 rounded-xl cursor-pointer hover:bg-green-500 duration-300">
+                SPIN
+            </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
